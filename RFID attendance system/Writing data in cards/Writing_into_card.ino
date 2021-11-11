021192602ac963865e85aa05e9c6fb52d657828b
Anshika Goyal <78787016+anshika-pixel@users.noreply.github.com>
2021-11-11 10:30:16 +0400

    Update Writing_into_card.ino

--- RFID attendance system/Writing data in cards/Writing_into_card.ino
+++ RFID attendance system/Writing data in cards/Writing_into_card.ino
@@ -3,10 +3,26 @@
 #define SS_PIN 10  
 #define RST_PIN 7  
 MFRC522 mfrc522(SS_PIN, RST_PIN);
-MFRC522::MIFARE_Key key;          
-int blockNum = 2;  //location in the tag where we want to save our data
-byte blockData [16]; //array to store the person's name
-String person_name;//variable to take the person's name
+MFRC522::MIFARE_Key key; 
+
+String person_name;//variable to take the person's name   
+String army_id;//variable to take the person's id
+String rank;//variable to take the person's rank
+String date_join;//variable to take the person's date of joining
+int battalion;//variable to take the person's battalion number
+
+int blockNum1 = 2;  //location in the tag where we want to save our data
+int blockNum2 = 3;  //location in the tag where we want to save our data
+int blockNum3 = 4;  //location in the tag where we want to save our data
+int blockNum4 = 5;  //location in the tag where we want to save our data
+int blockNum5 = 6;  //location in the tag where we want to save our data
+
+byte name_array [16]; //array to store the person's name
+byte id_array [16]; //array to store the person's id
+byte rank_array [16]; //array to store the person's rank
+byte date_array [16]; //array to store the person's date of joining
+byte battalion_array [16]; //array to store the person's battalion number
+
 byte bufferLen = 18;
 byte readBlockData[18];
 MFRC522::StatusCode status;
@@ -49,10 +65,54 @@ void loop() {
     //wait for the user input
   }
   person_name=Serial.readString();
-  person_name.getBytes(blockData,16);
+  person_name.getBytes(name_array,16);
   Serial.print("\n");
   Serial.println("Writing to Data to the card...");
-  WriteDataToBlock(blockNum, blockData);
+  WriteDataToBlock(blockNum1,name_array);
+
+  Serial.print("\n");
+  Serial.print("Enter the army ID of the person: ");
+  while(Serial.available()==0){
+    //wait for the user input
+  }
+  army_id=Serial.readString();
+  army_id.getBytes(id_array,16);
+  Serial.print("\n");
+  Serial.println("Writing to Data to the card...");
+  WriteDataToBlock(blockNum2,id_array);
+
+  Serial.print("\n");
+  Serial.print("Enter the rank of the person: ");
+  while(Serial.available()==0){
+    //wait for the user input
+  }
+  rank=Serial.readString();
+  rank.getBytes(rank_array,16);
+  Serial.print("\n");
+  Serial.println("Writing to Data to the card...");
+  WriteDataToBlock(blockNum3,rank_array);
+
+  Serial.print("\n");
+  Serial.print("Enter the date of joining of the person: ");
+  while(Serial.available()==0){
+    //wait for the user input
+  }
+  date_join=Serial.readString();
+  date_join.getBytes(date_array,16);
+  Serial.print("\n");
+  Serial.println("Writing to Data to the card...");
+  WriteDataToBlock(blockNum4,date_array);
+
+  Serial.print("\n");
+  Serial.print("Enter the battalion number of the person: ");
+  while(Serial.available()==0){
+    //wait for the user input
+  }
+  battalion=Serial.parseInt();
+  battalion_array[16]=battalion;
+  Serial.print("\n");
+  Serial.println("Writing to Data to the card...");
+  WriteDataToBlock(blockNum5,battalion_array);
 }
 
 void WriteDataToBlock(int blockNum, byte blockData[]) 
